6207c5a3e5e7f550abd4760353c4e81d50610f17
janitha-visna <visnajanitha@gmail.com>
2024-04-10 11:49:18 +0530

    store api keys in .env file to achive this we installed dependencies npm install --save-dev react-native-dotenv

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import * as Location from "expo-location";
+import { TEST_KEY } from "@env";
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
+  console.log(TEST_KEY);
 
   useEffect(() => {
     (async () => {
@@ -27,6 +31,7 @@ const App = () => {
     })();
   }, []);
 
+  console.log(TEST_KEY);
   if (Location) {
     console.log(location);
   }
