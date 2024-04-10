0da8a751e4156d8756355d777549e4fd09d794bc
janitha-visna <visnajanitha@gmail.com>
2024-04-10 22:17:32 +0530

    fetching data api data using api keys

--- App.js
+++ App.js
@@ -3,15 +3,30 @@ import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import * as Location from "expo-location";
-import { TEST_KEY } from "@env";
-
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+import { WEATHER_API_KEY } from "@env";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
-  console.log(TEST_KEY);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState(null); // Initialize with null instead of an empty array
+  const [lon, setLon] = useState(null); // Initialize with null instead of an empty array
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      //console data 
+      console.log(data);
+      setWeather(data);
+    } catch (e) {
+      setError("Could not fetch weather");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -21,20 +36,24 @@ const App = () => {
           setError("Permission to access location was denied");
           return;
         }
-        let locationData = await Location.getCurrentPositionAsync({});
-        setLocation(locationData);
+        let location = await Location.getCurrentPositionAsync({});
+        setLat(location.coords.latitude);
+        setLon(location.coords.longitude);
+        await fetchWeatherData();
       } catch (error) {
         setError("An error occurred while fetching location");
       } finally {
         setLoading(false); // Set loading to false once location data is fetched or an error occurs
       }
     })();
-  }, []);
+  }, [lat,lon]); // Empty dependency array to run only once on component mount
 
-  console.log(TEST_KEY);
-  if (Location) {
-    console.log(location);
-  }
+  console.log("Latitude:", lat); // Log latitude
+  console.log("Longitude:", lon); // Log longitude
+
+  // if (weather) {
+  //   console.log(weather);
+  // }
 
   return (
     <NavigationContainer>
