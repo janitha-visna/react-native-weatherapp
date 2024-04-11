05bfd9c58d9eebf2247b83118a61b7a4686815e4
janitha-visna <visnajanitha@gmail.com>
2024-04-11 08:18:40 +0530

    extract the logic to fetch data and make it as own hook

--- App.js
+++ App.js
@@ -4,52 +4,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import * as Location from "expo-location";
 import { WEATHER_API_KEY } from "@env";
+import { useGetWeather } from "./src/hooks/useGetWeather";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [weather, setWeather] = useState([]);
-  const [lat, setLat] = useState(null); // Initialize with null instead of an empty array
-  const [lon, setLon] = useState(null); // Initialize with null instead of an empty array
+  
+  const [loading,error,weather] = useGetWeather()
+  console.log(loading,error,weather)
 
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-      );
-      const data = await res.json();
-      //console data 
-      console.log(data);
-      setWeather(data);
-    } catch (e) {
-      setError("Could not fetch weather");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    (async () => {
-      try {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-          setError("Permission to access location was denied");
-          return;
-        }
-        let location = await Location.getCurrentPositionAsync({});
-        setLat(location.coords.latitude);
-        setLon(location.coords.longitude);
-        await fetchWeatherData();
-      } catch (error) {
-        setError("An error occurred while fetching location");
-      } finally {
-        setLoading(false); // Set loading to false once location data is fetched or an error occurs
-      }
-    })();
-  }, [lat,lon]); // Empty dependency array to run only once on component mount
-
-  console.log("Latitude:", lat); // Log latitude
-  console.log("Longitude:", lon); // Log longitude
+  // console.log("Latitude:", lat); // Log latitude
+  // console.log("Longitude:", lon); // Log longitude
 
   // if (weather) {
   //   console.log(weather);
